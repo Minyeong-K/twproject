da970d9ac9adb058a51646b0efaf2312305a561c
elisaint8 <52320233+elisaint8@users.noreply.github.com>
2020-02-09 20:10:43 +0900

    commit
    커밋커밋

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {Component, Fragment} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const name = '리랴릭서';
+    return (
+      <Fragment>
+        <div className="App">
+          <input type='text' value={name}/>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
